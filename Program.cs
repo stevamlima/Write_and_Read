60c69ab25d057b0594c8b6851032e997f488356f
FIC <stevamlima99@gmail.com>
2017-10-06 18:35:12 -0300

    inicio do projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace project_06_10_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
